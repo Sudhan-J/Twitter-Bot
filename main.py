7a7819051f5be7cd26e8fac497901b7a2a5a1743
Sudhan-J <sudhanj27@gmail.com>
2021-11-20 08:22:13 +0530

    added some features

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cfscrape
 import bs4 as bs
 import os
 import time
-from keep_alive import keep_alive
+from keep_alive import keep_alive # this is for replit only not needed if you run on the computer or own server.
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -45,12 +45,37 @@ def eth_info(url):
     change = change_elem[0].text.strip()
     return valeth,change
 
+def sol_info(url):
+    scraper = cfscrape.create_scraper()
+    scrap = scraper.get(url)
+    soup = bs.BeautifulSoup(scrap.text,"html.parser")
+    elemsol = soup.select('#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr:nth-child(5) > td:nth-child(4) > div > a')
+    change_elem = soup.select('#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr:nth-child(5) > td:nth-child(5) > span')
+    valsol = elemsol[0].text.strip()
+    change = change_elem[0].text.strip()
+    return valsol,change
+
+def Cardano_info(url):
+    scraper = cfscrape.create_scraper()
+    scrap = scraper.get(url)
+    soup = bs.BeautifulSoup(scrap.text,"html.parser")
+    elemada = soup.select('#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr:nth-child(6) > td:nth-child(4) > div > a')
+    change_elem = soup.select('#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr:nth-child(6) > td:nth-child(5) > span')
+    valada = elemada[0].text.strip()
+    change = change_elem[0].text.strip()
+    return valada,change
+
+
+
 def main():
     get_url = 'https://coinmarketcap.com/'
     btc,change = btc_info(get_url)
     eth,changeeth = eth_info(get_url)
-    api.update_status(f"#Crypto #Cryptocurrency #BTC #ETH\nPrices!\n\nBTC = {btc}\nChange = {change}\n\nETH = {eth}\nChange = {changeeth}\n\nFollow @pricebotcrypto for hourly update!")
-    
+    sol,changesol = sol_info(get_url)
+    ada, changeada = Cardano_info(get_url)
+    api.update_status(f"#Crypto #Cryptocurrency #BTC #ETH #Solana #ADA\n\nBTC = {btc}\nChange = {change}\n\nETH = {eth}\nChange = {changeeth}\n\nSolana = {sol}\nChange = {changesol}\n\nADA = {ada}\nChange = {changeada}\n\nFollow @pricebotcrypto for hourly update!")
+
+
 
 if __name__ == "__main__":
     while True:
